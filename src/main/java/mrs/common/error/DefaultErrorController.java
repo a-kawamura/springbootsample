02a3116731cbdfcee9b9a6b2d125b780a1294645
Akihiro Kawamura <a-kawamura@nyc.odn.ne.jp>
2020-07-05 10:06:31 +0900

    Add rest errorHandler to DefaultErrorController.

--- src/main/java/mrs/common/error/DefaultErrorController.java
+++ src/main/java/mrs/common/error/DefaultErrorController.java
@@ -4,41 +4,32 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
-import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.ServletWebRequest;
 
 @Controller
-public class DefaultErrorController implements ErrorController {
+public class DefaultErrorController extends AbstractErrorController {
 
-	@Autowired
-	private ErrorAttributes errorAttributes;
+	public DefaultErrorController(ErrorAttributes errorAttributes) {
+		super(errorAttributes);
+	}
 
 	@Override
 	public String getErrorPath() {
 		return "/error";
 	}
 
-	@RequestMapping("error")
+	@RequestMapping(path = "error", produces = MediaType.TEXT_HTML_VALUE)
 	@ResponseBody
 	public String handleError(HttpServletRequest request, Model model) {
-		ServletWebRequest webRequest = new ServletWebRequest(request);
-		Map<String, Object> errorAttributes = this.errorAttributes
-				.getErrorAttributes(webRequest,
-						ErrorAttributeOptions.defaults());
-
-		Exception e = (Exception) request.getAttribute("exception");
-		if (e != null) {
-			errorAttributes.put("Exception", e.getClass().getCanonicalName());
-			errorAttributes.put("Exception message", e.getMessage());
-		}
-
+		Map<String, Object> errorAttributes = getErrorAttributes(request);
 		StringBuilder errorDetails = new StringBuilder();
 		errorAttributes.forEach((attribute, value) -> {
 			errorDetails.append("<tr><td>").append(attribute)
@@ -49,5 +40,24 @@ public class DefaultErrorController implements ErrorController {
 				"<html><head><style>td{vertical-align:top;border:solid 1px #666;}</style>"
 						+ "</head><body><h2>Error Page</h2><table>%s</table></body></html>",
 				errorDetails.toString());
+	}
+
+	@RequestMapping(path = "error")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> handleErrorForRest(
+			HttpServletRequest request, Model model) {
+		Map<String, Object> body = getErrorAttributes(request);
+		return new ResponseEntity<>(body, getStatus(request));
+	}
+
+	private Map<String, Object> getErrorAttributes(HttpServletRequest request) {
+		Map<String, Object> errorAttributes = getErrorAttributes(request,
+				ErrorAttributeOptions.defaults());
+		Exception e = (Exception) request.getAttribute("exception");
+		if (e != null) {
+			errorAttributes.put("exception", e.getClass().getCanonicalName());
+			errorAttributes.put("message", e.getMessage());
+		}
+		return errorAttributes;
 	}
 }
