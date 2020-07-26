c3adcb5b21508e97c7d63ba4fcb4a576ade4969b
Akihiro Kawamura <a-kawamura@nyc.odn.ne.jp>
2020-07-26 18:09:58 +0900

    Amend status of errorHandler.

--- src/main/java/mrs/common/error/DefaultErrorController.java
+++ src/main/java/mrs/common/error/DefaultErrorController.java
@@ -2,17 +2,23 @@ package mrs.common.error;
 
 import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Controller
 public class DefaultErrorController extends AbstractErrorController {
@@ -28,7 +34,8 @@ public class DefaultErrorController extends AbstractErrorController {
 
 	@RequestMapping(path = "error", produces = MediaType.TEXT_HTML_VALUE)
 	@ResponseBody
-	public String handleError(HttpServletRequest request, Model model) {
+	public String handleError(HttpServletRequest request,
+			HttpServletResponse response, Model model) {
 		Map<String, Object> errorAttributes = getErrorAttributes(request);
 		StringBuilder errorDetails = new StringBuilder();
 		errorAttributes.forEach((attribute, value) -> {
@@ -36,6 +43,7 @@ public class DefaultErrorController extends AbstractErrorController {
 					.append("</td><td><pre>").append(value)
 					.append("</pre></td></tr>");
 		});
+		response.setStatus(getStatus(request).value());
 		return String.format(
 				"<html><head><style>td{vertical-align:top;border:solid 1px #666;}</style>"
 						+ "</head><body><h2>Error Page</h2><table>%s</table></body></html>",
@@ -54,6 +62,22 @@ public class DefaultErrorController extends AbstractErrorController {
 				ErrorAttributeOptions.defaults());
 		Exception e = (Exception) request.getAttribute("exception");
 		if (e != null) {
+			if ((e instanceof MethodArgumentTypeMismatchException)
+					|| (e instanceof MethodArgumentNotValidException)
+					|| (e instanceof BindException)) {
+				request.setAttribute(RequestDispatcher.ERROR_STATUS_CODE,
+						HttpStatus.BAD_REQUEST.value());
+				errorAttributes.put("status", HttpStatus.BAD_REQUEST.value());
+				errorAttributes.put("error",
+						HttpStatus.BAD_REQUEST.getReasonPhrase());
+			} else {
+				request.setAttribute(RequestDispatcher.ERROR_STATUS_CODE,
+						HttpStatus.INTERNAL_SERVER_ERROR.value());
+				errorAttributes.put("status",
+						HttpStatus.INTERNAL_SERVER_ERROR.value());
+				errorAttributes.put("error",
+						HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+			}
 			errorAttributes.put("exception", e.getClass().getCanonicalName());
 			errorAttributes.put("message", e.getMessage());
 		}
